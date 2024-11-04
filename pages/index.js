14b8e5bc2b77a96d1aec9c174978c6f99844b7fa
Maryam Shahvalizadeh <Shahvalizadehmaryam@gmail.com.>
2024-11-04 10:10:47 +0330

    removed unnecessary codes which is primary app codes.

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+     <h3>pre rendering and data fetching in next.js</h3>
+    </div>
+  )
+}
