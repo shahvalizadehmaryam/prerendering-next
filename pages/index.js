e59d547d141df07bcc98702274bc4c38b8d01fb1
Maryam Shahvalizadeh <Shahvalizadehmaryam@gmail.com.>
2024-11-05 11:24:41 +0330

    some minor change.

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,25 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className={styles.container}>
-     <h3>pre rendering and data fetching in next.js</h3>
+      <h3>pre rendering and data fetching in next.js</h3>
+      <ul>
+        {posts.map((postItem) => (
+          <li key={postItem.id}>{postItem.title}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
+}
+// this function run at build time.
+export async function getStaticProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+  console.log(data);
+  return {
+    props: {
+      posts: data,
+    },
+  };
 }
