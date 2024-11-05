71267211f50941d5230c939477491205e01671b4
Maryam Shahvalizadeh <Shahvalizadehmaryam@gmail.com.>
2024-11-05 13:05:10 +0330

    fetched user detail part with getStaticProps and getStaticPaths.

--- pages/users/[userId].js
+++ pages/users/[userId].js
@@ -1,5 +1,35 @@
-const UserDetail = () => {
-  return <div>UserDetail</div>;
+const UserDetail = ({ data }) => {
+  return (
+    <div>
+      <h3>user detail</h3>
+      <p> {data.name}</p>
+      <p> {data.email}</p>
+    </div>
+  );
 };
 
 export default UserDetail;
+export async function getStaticPaths() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  const paths = data.map((user) => ({
+    params: { userId: user.id.toString() },
+  }));
+  console.log(paths);
+  return {
+    paths,
+    fallback: false,
+  };
+}
+export async function getStaticProps(context) {
+  const { params } = context;
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${params.userId}`
+  );
+  const data = await res.json();
+  return {
+    props: {
+      data,
+    },
+  };
+}
