347dce417c8f7a869c56f2debe425a4929d92220
Maryam Shahvalizadeh <Shahvalizadehmaryam@gmail.com.>
2024-11-06 08:50:04 +0330

    comment added to userId file.

--- pages/users/[userId].js
+++ pages/users/[userId].js
@@ -12,6 +12,7 @@ export default UserDetail;
 export async function getStaticPaths() {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json();
+  // id must be set as a string
   const paths = data.map((user) => ({
     params: { userId: user.id.toString() },
   }));
